b9a0f93fb982bfb6808ecd18234685972c95eff2
Shubham Gupta <39215648+tech-shubham@users.noreply.github.com>
2019-05-27 12:27:39 +0530

    Add files via upload

---
+++ main/main.py
@@ -0,0 +1,30 @@
+from ota_update.main.ota_updater import OTAUpdater
+from machine import Pin
+
+
+
+ def download_and_install_update_if_available():
+     o = OTAUpdater('https://github.com/tech-shubham/ESP32_OTA')
+     o.download_and_install_update_if_available('Shubham', 'shubham1234')
+
+
+ def start():
+     # your custom code goes here. Something like this: ...
+     # from main.x import YourProject
+     # project = YourProject()
+     # ...
+
+
+ def boot():
+     download_and_install_update_if_available()
+     start()
+
+
+ boot()
+
+
+led = Pin(2, Pin.OUT)
+
+while True:
+  led.value(not led.value())
+  time.sleep(0.5)
